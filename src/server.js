4276947040f569b36df56514de7f42f99d56fb5a
hayrus1234 <suryah609@gmail.com>
2024-07-12 12:33:21 +0530

    Index updated

--- src/server.js
+++ src/server.js
@@ -45,7 +45,7 @@ server.listen(PORT, () => {
 
 //health check
 app.get('/health', (req, res) => {
-  res.send('Health Status OK ')
+  res.send('Health Status  ')
 })
 
 //user page for displaying all connected users with the nickname they enetered
